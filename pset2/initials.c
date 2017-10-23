9471b6e0c1ac40952ff551f00dd38a679a30e0c1
dnguyen22 <don.nguyen22@gmail.com>
2017-10-23 10:59:56 -0500

    Print capitalized first letter

--- pset2/initials.c
+++ pset2/initials.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
+    // Get name from user
     string name = get_string();
-    //printf("String inputted: %s\n", name);
 
+    // Loop through user input
     for (int i = 0, n = strlen(name); i < n; i++)
     {
-        printf("%c\n", name[i]);
+        // Ignore spaces
+        if (name[i] != ' ')
+        {
+            // Print capitalized letter if first in string or after a space
+            if (i == 0 || name[i-1] == ' ')
+            {
+                printf("%c", toupper(name[i]));
+            }
+        }
     }
+
+    // Create new line after looping through input
+    printf("\n");
 }
